c54fd26bc3b4b6eb966291c33df22b4f68c9a1bd
Selamawitl lemma <oneiconforlife26@gmail.com>
2020-08-20 12:33:42 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -3,6 +3,6 @@ def say_hello(person_name)
   puts "Hello #{person_name}!"
 end
 
-def say_hello(person_name ="Ruby programmer")
-  puts "Hello #{person_name}!"
+def say_hello(Ruby_programmer)
+  puts "Hello #{Ruby_programmer}!"
 end
